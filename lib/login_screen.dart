20ada3a409b0b1aa3fc9b3834750e5356839fba8
AMANDEEP-DAS <dasaman1997@gmail.com>
2021-09-12 01:08:33 +0530

    login screen done

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,8 +6,85 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+                colors: [
+                  Colors.white,
+                  Colors.cyan,
+                ],
+                stops: [0.0, 1.0],
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                tileMode: TileMode.repeated
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset(
+                'assets/hyre_logo.jpeg',
+                fit: BoxFit.cover,
+                height: 70,
+                width: 200,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: 'Enter your username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Material(
+                color: Colors.deepOrange,
+                elevation: 5,
+                borderRadius: BorderRadius.circular(15),
+                child: MaterialButton(
+                  minWidth: MediaQuery.of(context).size.width,
+                  height: 42,
+                  onPressed: () {
 
+                  },
+                  child: Text(
+                    'LOG IN',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Dont have an account?'),
+                SizedBox(width: 5,),
+                Text('Sign Up',style: TextStyle(color: Colors.blue),)
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
