bfe8407ddf15c7103c612fc2b2e83a6e88ec4927
AMANDEEP-DAS <dasaman1997@gmail.com>
2021-09-12 21:44:27 +0530

    small changes

--- lib/website_dev_screen.dart
+++ lib/website_dev_screen.dart
@@ -41,7 +41,7 @@ class WebScreen extends StatelessWidget {
                       width: MediaQuery.of(context).size.width/2.4,
                       decoration: BoxDecoration(
                         image: DecorationImage(
-                          image: NetworkImage('https://source.unsplash.com/1000x1000/?boy'),
+                          image: NetworkImage('https://source.unsplash.com/1000x1000/?girl'),
                           fit: BoxFit.fill,
                         ),
                       ),
@@ -56,64 +56,7 @@ class WebScreen extends StatelessWidget {
                         children: [
                           Text('FRONT-END DEV.',style: TextStyle(fontWeight: FontWeight.bold),),
                           SizedBox(height: 10,),
-                          Text('CRISTIANO RONALDO',style: TextStyle(fontSize: 10),),
-                          SizedBox(height: 10,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(Icons.star,color: Colors.yellow,),
-                                  Text('4.7')
-                                ],
-                              ),
-                              Text(' ₹15000',style: TextStyle(color: Colors.red),)
-                            ],
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(height: 35,),
-              Container(
-                height: 120,
-                width: MediaQuery.of(context).size.width/1.1,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 120,
-                      width: MediaQuery.of(context).size.width/2.4,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage('https://source.unsplash.com/1000x1000/?person'),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(right: 10),
-                      height: 120,
-                      width: MediaQuery.of(context).size.width/2.4,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text('BACK-END DEV.',style: TextStyle(fontWeight: FontWeight.bold),),
-                          SizedBox(height: 10,),
-                          Text('KYLIAN MBAPPE',style: TextStyle(fontSize: 10),),
+                          Text('SALEMA AAFIFA',style: TextStyle(fontSize: 10),),
                           SizedBox(height: 10,),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -124,7 +67,7 @@ class WebScreen extends StatelessWidget {
                                   Text('4.5')
                                 ],
                               ),
-                              Text(' ₹8000',style: TextStyle(color: Colors.red),)
+                              Text('\$ 10.00',style: TextStyle(color: Colors.red),)
                             ],
                           )
                         ],
@@ -168,9 +111,9 @@ class WebScreen extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          Text('WEBSITE DESIGNER',style: TextStyle(fontWeight: FontWeight.bold),),
+                          Text('BACK-END DEV.',style: TextStyle(fontWeight: FontWeight.bold),),
                           SizedBox(height: 10,),
-                          Text('ERLING HAALAND',style: TextStyle(fontSize: 10),),
+                          Text('NICK TYSON',style: TextStyle(fontSize: 10),),
                           SizedBox(height: 10,),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -178,10 +121,67 @@ class WebScreen extends StatelessWidget {
                               Row(
                                 children: [
                                   Icon(Icons.star,color: Colors.yellow,),
-                                  Text('4.0')
+                                  Text('4.2')
                                 ],
                               ),
-                              Text(' ₹10000',style: TextStyle(color: Colors.red),)
+                              Text('\$ 8.00',style: TextStyle(color: Colors.red),)
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 35,),
+              Container(
+                height: 120,
+                width: MediaQuery.of(context).size.width/1.1,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 120,
+                      width: MediaQuery.of(context).size.width/2.4,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage('https://source.unsplash.com/1000x1000/?boy'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(right: 10),
+                      height: 120,
+                      width: MediaQuery.of(context).size.width/2.4,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text('WEBSITE DESIGNER',style: TextStyle(fontWeight: FontWeight.bold),),
+                          SizedBox(height: 10,),
+                          Text('ARAF SHEIKH',style: TextStyle(fontSize: 10),),
+                          SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.star,color: Colors.yellow,),
+                                  Text('3.2')
+                                ],
+                              ),
+                              Text('\$ 8.00',style: TextStyle(color: Colors.red),)
                             ],
                           )
                         ],
